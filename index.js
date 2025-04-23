f8e23bbd2c9ee230e0d0525c225cfa2885d59f05
Pablo Villar Morón <pablo.villar.moron@gmail.com>
2025-04-23 15:42:50 +0200

    primer-lab-tipos-datos

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+let newString= `${s1} ${s2} ${s3} ${s5} ${s4}`
 
 // Print out the concatenated string
 
-
+console.log(newString)
 
 
 /*******************************************
@@ -22,11 +22,12 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+let part1ToUpper= part1.slice(0, part1.length -1) + part1[part1.length-1].toUpperCase()
+let part2ToUpper= part2.slice(0, part2.length -1) + part2[part2.length-1].toUpperCase()
 
 // Print the cameLtaiL-formatted string
 
-
+console.log(part1ToUpper+part2ToUpper)
 
 
 /*******************************************
@@ -35,11 +36,11 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+let tip = billTotal * 15 / 100
 
 // Print out the tipAmount
 
-
+console.log(tip)
 
 
 /*******************************************
@@ -47,10 +48,10 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+let randomNum = Math.random() * 10
 
 // Print the generated random number
-
+console.log(randomNum)
 
 
 /*******************************************
@@ -62,15 +63,24 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
-
+//false//
 const expression2 = a || b;
-
+//true//
 const expression3 = !a && b;
-
+//false//
 const expression4 = !(a && b);
-
+//true//
 const expression5 = !a || !b;
-
+//true//
 const expression6 = !(a || b);
-
+//false//
 const expression7 = a && a;
+//true//
+
+console.log(expression1)
+console.log(expression2)
+console.log(expression3)
+console.log(expression4)
+console.log(expression5)
+console.log(expression6)
+console.log(expression7)
